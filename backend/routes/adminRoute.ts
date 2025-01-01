ec9469a3d75fcd68e76ed165c448be461ad75c5f
shabinx30 <shabeensharih@gmail.com>
2025-01-01 20:37:11 +0530

    server structure added

--- backend/routes/adminRoute.ts
+++ backend/routes/adminRoute.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import adminController from "../controllers/adminController"
 const router =  express.Router()
 
 export default router
--- backend/routes/userRoute.ts
+++ backend/routes/userRoute.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import userController from '../controllers/userController'
 const router = express.Router()
 
 
