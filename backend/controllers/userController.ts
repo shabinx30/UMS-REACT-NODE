8f43f1a34320270fcb8cf72495ba6572e91f7b6a
shabinx30 <shabeensharih@gmail.com>
2025-01-02 13:02:13 +0530

    user adding functionality added

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import * as userModel from '../models/userModel'
+
+const addUser = async (req: Request, res: Response) : Promise<void> => {
+    try {
+        const { name, profile, email, password } = req.body
+        if(!name || !email || !password){
+            res.status(400).json({ error: "Name, email, password is requied!" });
+            return 
+        }
+        const user = await userModel.addUser(name, profile || '', email, password)
+        res.status(201).json({ user })
+    } catch (error) {
+        res.status(500).json({ error: "Error while adding the user!" });
+    }
+}
 
 export default {
-    
+    addUser
 }
--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -1,10 +1,10 @@
 import pool from "../db";
 import bcrypt from "bcrypt";
 
-export interface User {
+interface User {
   id?: number;
   name: string;
-  profile: string;
+  profile?: string;
   email: string;
   password: string;
 }
@@ -13,15 +13,16 @@ const HashPassword = async (password: string): Promise<string> => {
   return await bcrypt.hash(password, 10);
 };
 
-const addUser = async (name:string, profile: string, email: string, password: string): Promise<string> => {
-    const hashedPassword = await HashPassword(password)
-    const query = 'insert into users (name, profile, email, password) values($1, $2, $3, $4) returning id, name, profile, email, created_at'
-    const values = [name, profile, email, hashedPassword]
-    const result = await pool.query(query,values)
-    return result.rows[0];
+const addUser = async (name:string, profile: string, email: string, password: string): Promise<User> => {
+  const hashedPassword = await HashPassword(password)
+  const query = 'insert into users (name, profile, email, password) values($1, $2, $3, $4) returning id, name, profile, email, created_at'
+  const values = [name, profile, email, hashedPassword]
+  const result = await pool.query(query,values)
+  return result.rows[0];
 }
 
-export default{ 
-    HashPassword, 
-    addUser 
+export { 
+  User,
+  HashPassword, 
+  addUser 
 };
