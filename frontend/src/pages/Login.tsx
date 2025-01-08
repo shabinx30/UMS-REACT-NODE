ef72ca31303295b81ea5a2fe7b9287fce9fe914f
shabinx30 <shabeensharih@gmail.com>
2025-01-09 00:00:55 +0530

    state added for login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login : React.FC = () => {
 
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const validate = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name] : value
+    }))
+  }
 
   return (
     <>
@@ -25,6 +37,7 @@ const Login : React.FC = () => {
                     type="email"
                     name="email"
                     id="email"
+                    onChange={validate}
                     className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="example@company.com"
                     required
