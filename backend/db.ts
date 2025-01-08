51d87f68db6142abf87d8ee8aac33a46e72379c6
shabinx30 <shabeensharih@gmail.com>
2025-01-08 23:25:38 +0530

    code clean up 2

--- backend/db.ts
+++ backend/db.ts
@@ -1,6 +1,5 @@
 const { Pool } = require("pg");
 
-// console.log('test of db.ts',process.env.DB_HOST)
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -9,19 +8,5 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT) || 5432,
 });
-
-// const connect = async () => {
-//   db.connect()
-//     .then(() => console.log("Database connected"))
-//     .catch((err: any) => console.error("Connection error:", err));
-// };
-
-// db.query('select * from users', (err:any,res:any) => {
-//   if(!err){
-//     console.log(res.rows[0])
-//   }else{
-//     console.log('db error',err.message)
-//   }
-// })
 
 export default pool
--- backend/server.ts
+++ backend/server.ts
@@ -7,24 +7,6 @@ import cors from "cors";
 import pool from "./db";
 
 
-// const { Pool } = require("pg");
-
-// const db = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: String(process.env.DB_PASSWORD),
-//   port: Number(process.env.DB_PORT) || 5432,
-// });
-
-// const connect = async () => {
-  pool.connect()
-    .then(() => console.log("Database connected"))
-    .catch((err: any) => console.error("Connection error:", err));
-// };
-
-
-
 const app = express();
 app.use(cors({ origin: "http://localhost:5173" }));
 
@@ -37,8 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/admin", adminRoute);
 app.use("/", userRoute);
 
+
 //data base connection
-// db.connect()
+pool.connect()
+.then(() => console.log("Database connected"))
+.catch((err: any) => console.error("Connection error:", err));
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`server is running on ${process.env.PORT || 4000}`);
