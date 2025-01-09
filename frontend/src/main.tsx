df836183118bfa4b83dbe3448c93d0e7a836d9fb
shabinx30 <shabeensharih@gmail.com>
2025-01-09 17:46:07 +0530

    reduxt states are added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,2 +1,58 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
+const INITIAL_STATE = {
+    token: null,
+    user: null
+}
+
+const user = createSlice({
+    name: "auth",
+    initialState: INITIAL_STATE,
+    reducers: {
+        login: (state, action) => {
+            state.token = action.payload.token,
+            state.user = action.payload.user
+        },
+        logout: (state) => {
+            state.token = null,
+            state.user = null
+        }
+    }
+})
+
+
+const allUser = createSlice({
+    name: "users",
+    initialState: [],
+    reducers: {
+        addData: (_, action) => {
+            return action.payload
+        }
+    }
+})
+
+const handleModal = createSlice({
+    name: "modal",
+    initialState: { index: null, status: false },
+    reducers: {
+        modalcontrol: (state, action) => {
+            state.index = action.payload.index,
+                state.status = action.payload.status
+        }
+    }
+})
+
+const store = configureStore({
+    reducer: {
+        auth: user.reducer,
+        users: allUser.reducer,
+        modal: handleModal.reducer
+    }
+})
+
+
+export const { login, logout } = user.actions
+export const { addData } = allUser.actions
+export const { modalcontrol } = handleModal.actions
+export default store
