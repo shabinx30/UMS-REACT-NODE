dbf0c99da70b1a5ecd75b7df5f454797c714e883
shabinx30 <shabeensharih@gmail.com>
2025-01-14 21:30:59 +0530

    stucture changed in component folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Login from "./pages/users/Login";
-import SignUp from "./pages/users/SignUp";
-import Profile from "./pages/users/Profile";
+import Login from "./pages/user/Login";
+import SignUp from "./pages/user/SignUp";
+import Profile from "./pages/user/Profile";
 import AdminLogin from "./pages/admin/AdminLogin";
 import Users from "./pages/admin/Users";
 
 //Protection
-import UserAuth from "./components/UserAuth";
-import UserVeriAuth from "./components/UserVeriAuth";
-import AdminAuth from "./components/AdminAuth";
-import AdminVeriAuth from "./components/AdminVeriAuth";
+import UserAuth from "./components/user/UserAuth";
+import UserVeriAuth from "./components/user/UserVeriAuth";
+import AdminAuth from "./components/admin/AdminAuth";
+import AdminVeriAuth from "./components/admin/AdminVeriAuth";
 
 function App() {
   return (
