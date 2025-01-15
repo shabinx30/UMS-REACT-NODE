8c6c5a62c4bbc9a9b77b90d58a67007c2d105fb0
shabinx30 <shabeensharih@gmail.com>
2025-01-15 09:50:17 +0530

    logout button added in admin side

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,47 @@
-import React,{ useEffect } from 'react'
+import { jwtDecode } from "jwt-decode";
+import React from "react";
+import { logout } from "../redux/store";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
+const Navbar: React.FC = () => {
+  interface DecodedToken {
+    userId: string | number;
+    role: string;
+  }
 
+  const token = localStorage.getItem("jwt");
+  let data: DecodedToken | null = null;
+  if (token) {
+    data = jwtDecode<DecodedToken>(token);
+    console.log("deconded form app", data);
+  }
+  console.log(data == null ? "no user founded" : "user founded");
 
-const Navbar : React.FC = () => {
+  // for logout
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        
-    },[])
+  const handleLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem("jwt");
+    navigate("/admin");
+  };
 
   return (
     <>
-        <div className='fixed z-10 bg-gray-900 justify-between p-5 w-full'>
-            <h1 className='text-primary font-bold text-3xl'>Hello</h1>
-            <img src="" alt="" />
-        </div>
+      <div className="fixed flex z-10 bg-gray-900 justify-between p-5 w-full">
+        <h1 className="text-primary font-bold text-3xl">Hello</h1>
+        {/* <img src="" alt="" /> */}
+        {data != null && data.role == 'admin' ? <button
+          className="bg-red-600 text-white rounded-xl px-3 pb-2 pt-1"
+          onClick={handleLogout}
+        >
+          Logout
+        </button> : <></>}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ frontend/src/components/admin/Modal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Modal: React.FC = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Modal
