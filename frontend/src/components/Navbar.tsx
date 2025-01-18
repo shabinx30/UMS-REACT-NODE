d29259fb9b50f6afc2ca6d865c670803073cdd5f
shabinx30 <shabeensharih@gmail.com>
2025-01-18 18:35:46 +0530

    admin logout button bug solved

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { jwtDecode } from "jwt-decode";
 import React from "react";
 import { logout } from "../redux/store";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   interface DecodedToken {
@@ -28,12 +28,15 @@ const Navbar: React.FC = () => {
     navigate("/admin");
   };
 
+  const location = useLocation()
+  console.log(location)
+
   return (
     <>
       <div className="fixed flex z-10 bg-gray-900 justify-between p-5 w-full">
         <h1 className="text-primary font-bold text-3xl">Hello</h1>
         {/* <img src="" alt="" /> */}
-        {data != null && data.role == 'admin' ? <button
+        {data !== null && data.role === 'admin' && location.pathname === '/admin/users' ? <button
           className="bg-red-500 text-white rounded-xl px-3 pb-2 pt-1"
           onClick={handleLogout}
         >
